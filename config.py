9639acad3975af677574af4d7195a70b5c5c66d6
Renate Schmidt <rschmidt0970@yahoo.com>
2026-05-31 12:37:37 -0400

    Second attempt to delpy via Render

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class TestConfig:
     DEBUG = True
     CACHE_TYPE = 'SimpleCache'
 
-class ProductConfig:
+class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     CACHE_TYPE = 'SimpleCache'
 
